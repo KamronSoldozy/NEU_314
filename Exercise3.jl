d48f57bcc00a734861c63ac2bf437e53d196d146
Kamron Soldozy <ksoldozy95@gmail.com>
2019-10-01 23:15:19 -0400

    Plot rotation
    Added the ability to circularly "rotate" matrices of pixels by a specified number of degrees

--- Exercise3.jl
+++ Exercise3.jl
@@ -27,3 +27,33 @@ subplot(1, 2, 2);
 imshow(image2);
 title("Revised el-captain")
 axis("off")
+
+
+function rotateChannel(channel, numberpixUp)
+    numberofRows = length(channel[:,1])
+    returnChannel = zeros(typeof(channel[1]), length(channel[:,1]), length(channel[1,:]))
+    for index = 1:length(channel[:,1]) #For each row in channel
+    var = index+numberpixUp
+        while var > numberofRows #adjusts illegal indices
+            var = var - numberofRows
+        end
+        returnChannel[index,:] = channel[var,:]#Thus, only indices within real bounds are added
+    end
+    return returnChannel;
+end
+
+
+
+figure(figsize=[10, 3])
+subplot(1, 2, 1);
+imshow(img);
+axis("off")
+title("Original el-capitan")
+image3 = zeros(360, 640, 3)
+image3[:,:,1] =rotateChannel(r,180);
+image3[:,:,2] =g;
+image3[:,:,3] =b;
+subplot(1, 2, 2);
+imshow(image3);
+title("Revised el-captain by 180 pixels of r")
+axis("off")
