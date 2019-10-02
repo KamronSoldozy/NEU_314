767b4eda70312ec234678e3824fdc76251226da2
Kamron Soldozy <ksoldozy95@gmail.com>
2019-10-01 22:50:41 -0400

    Plotting
    Added the code to plot the modified el-capitan and added the png file corresponding to it as well.

--- Exercise3.jl
+++ Exercise3.jl
@@ -8,5 +8,22 @@ function partb(str)
     r = img[:,:,1]
     g = img[:,:,2]
     b = img[:,:,3]
+    #println("BUG BUG BUG")
+    println("reset with git reset --mixed")
     return r, g, b;
 end
+
+r, g, b = partb("el-capitan.png")
+image2 = zeros(360, 640, 3)
+image2[:,:,1] =g;
+image2[:,:,2] =b;
+image2[:,:,3] =r;
+figure(figsize=[10, 3])
+subplot(1, 2, 1);
+imshow(img);
+axis("off")
+title("Original el-capitan")
+subplot(1, 2, 2);
+imshow(image2);
+title("Revised el-captain")
+axis("off")
